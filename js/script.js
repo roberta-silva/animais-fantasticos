d2361b7a300a1c57f7d6a691405f9c8ba89c2fad
Roberta Silva <robertadasilva.contato@gmail.com>
2025-12-26 17:50:48 -0300

    adiciona tooltip area contato

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 import initScrollSuave from "./modules/scroll-suave.js";
-
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-
 import initAccordion from "./modules/accordion.js";
-
 import iniTabNav from "./modules/tabnav.js";
-
 import iniModal from "./modules/modal.js";
+import iniTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 iniTabNav();
 iniModal();
+iniTooltip();
