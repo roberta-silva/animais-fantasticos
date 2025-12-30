c4255e6f2d80ef10b9ae277f70d2eb325dfae8ed
Roberta Silva <robertadasilva.contato@gmail.com>
2025-12-30 17:24:28 -0300

    substitui section estática por conteúdo dinâmico via fetch (API fake)

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import iniModal from "./modules/modal.js";
 import iniTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +17,5 @@ iniModal();
 iniTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
