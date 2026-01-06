51d7fa698595263bac2bc83f5fb64f3b9c95e4a9
Roberta Silva <robertadasilva.contato@gmail.com>
2026-01-06 17:55:39 -0300

    instala ESLint e ajusta regras

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,17 +3,16 @@ export default function initModal() {
   const botaoFechar = document.querySelector('[data-modal="fechar"]');
   const containerModal = document.querySelector('[data-modal="container"]');
 
+  function toggleModal(event) {
+    event.preventDefault();
+    containerModal.classList.toggle("ativo");
+    // console.log(event);
+  }
+
+  function cliqueForaModal(event) {
+    if (event.target === this) toggleModal(event);
+  }
   if (botaoAbrir && botaoFechar && containerModal) {
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle("ativo");
-      // console.log(event);
-    }
-
-    function cliqueForaModal(event) {
-      if (event.target === this) toggleModal(event);
-    }
-
     botaoAbrir.addEventListener("click", toggleModal);
     botaoFechar.addEventListener("click", toggleModal);
     containerModal.addEventListener("click", cliqueForaModal);
